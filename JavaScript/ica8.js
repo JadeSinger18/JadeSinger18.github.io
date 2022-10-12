6d3d9b8c0104ed9174b2f456b8d71c52c07d5ddf
JadeSinger18 <89484528+JadeSinger18@users.noreply.github.com>
2022-10-12 11:46:11 -0600

    Update

---
+++ JavaScript/ica8.js
@@ -0,0 +1,11 @@
+//let body = document.querySelector('body');//
+//body.addEventListener('click', giveAlert);//
+
+//function giveAlert() {
+    //alert('this is an alert');
+//}
+
+
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
