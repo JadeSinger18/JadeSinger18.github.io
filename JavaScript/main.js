53120ca768f2eedb57e340f21cf0cf9e46f65dc5
JadeSinger18 <89484528+JadeSinger18@users.noreply.github.com>
2022-10-13 17:00:58 -0600

    Update

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -9,11 +9,11 @@ function randomValueFromArray(array){
 }
 
 
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const storyText = "It was 64 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in awe for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx:, weighs 300 pounds and it was a cold day.";
 
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const insertY = ["the soup kitchen", "Disneyland", "the White House"];
-const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const insertX = ["Newt", "Barry", "Sky"];
+const insertY = ["Shake Shack", "Pizza Pie", "UMC"];
+const insertZ = ["violently threw up the chicken nuggets", "melted into the cheese fries they were eating", "turned into a pizza"];
 
 
 var newStory = storyText;
