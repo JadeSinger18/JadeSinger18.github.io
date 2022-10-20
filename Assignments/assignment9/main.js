cb600ea46bae6125cc0628d31b0baf2bc4a5fbc9
JadeSinger18 <89484528+JadeSinger18@users.noreply.github.com>
2022-10-20 16:49:08 -0600

    Assignment9

---
+++ Assignments/assignment9/main.js
@@ -0,0 +1,46 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const images = ["../img/Sunset1.JPG", "../img/Sunset2.JPG", "../img/Sunset3.JPG", "../img/Rainbow.JPG", "../img/Lake.JPG"]
+
+/* Declaring the alternative text for each image file */
+
+const alts = {
+    'Sunset1.JPG' : 'Sunset with dark mountains',
+    'Sunset2.JPG' : 'Sunset with a tree',
+    'Sunset3.JPG' : 'Sunset with lots of orange and yellow and lake',
+    'Rainbow.JPG' : 'A double rainbow',
+    'Lake.JPG' : 'Lake with sun'
+    }
+
+/* Looping through images */
+
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
