d4870a64aaa17fead0373b0444ff2ede133e06b7
JadeSinger18 <89484528+JadeSinger18@users.noreply.github.com>
2022-11-03 15:41:58 -0600

    update

--- Assignments/assignment11/11.js
+++ Assignments/assignment11/11.js
@@ -1,5 +1,5 @@
-//window.addEventListener('load', getQuote);
-
+const twitterButton = document.querySelector('#js-tweet');
+const spinner =document.querySelector('#js-spinner');
 const quoteButton = document.querySelector('.new-quote');
 quoteButton.addEventListener('click', getQuote);
 
@@ -7,24 +7,40 @@ const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 
 async function getQuote() {
     let text = await fetch(endpoint);
-    let response = await text.text();
+    let  response = await text.text();
     
     let json_response = JSON.parse(response);
     console.log(json_response['message']);
     
     
     displayQuote(json_response['message']);
+    setTweetButton(json_response['message']);
 
+    spinner.classList.remove('hidden');
+    quoteButton.disabled = true;
+    try {
+    const response = await fetch(endpoint)
+    if (!response.ok) {
+    throw Error(response.statusText);
+   }
+    const json = await response.json();
+    displayQuote(json_message);
+    setTweetButton(json_response);
+   } catch {
+    alert('Tweet your quote to your friends!');
+   } finally {
+    quoteButton.disabled = false;
+    spinner.classList.add('hidden');
+        }
 }
 
 function displayQuote(x) {
-    //const quoteBox = document.querySelector('#js-quote-text');
-    //console.log('display quote working');
-    //const textMessage = document.createTextMode9(x);
-
-    //quoteBox.appendChild(textMessage);
-
     document.getElementById('js-quote-text').textContent = x;
 }
 
+function setTweetButton(quote) {
+    twitterButton.setAttribute('href', `https://twitter.com/share?text=${quote} - Donald Trump`);
+  }
+
 getQuote();
+setTweetButton();
