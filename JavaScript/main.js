33842cf6cf0c971d474ebdbe2a2ed2354ab7109b
JadeSinger18 <89484528+JadeSinger18@users.noreply.github.com>
2022-12-06 10:21:00 -0700

    update

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,7 +2,7 @@ const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
 /* Declaring the array of image filenames */
-const images = ["../Img/Reeses.jpg", "../Img/Monkey.jpg", "../Img/Turtle.jpg", "../Img/Moon.jpg", "../Img/Penguin.jpg", "../Img/Space.JPG", "../Img/SpiderMan.jpeg", "../Img/Portrait.jpeg", "../Img/Lamp.jpeg", "../Img/MoonLander.PNG", "../Img/Telescope.PNG"]
+const images = ["../Img/Reeses.jpg", "../Img/Monkey.jpg", "../Img/Turtle.jpg", "../Img/Moon.jpg", "../Img/Telescope.PNG", "../Img/Earth.jpg", "../Img/Portrait.jpeg", "../Img/Penguin.jpg", "../Img/MoonLander.PNG", "../Img/Space.JPG", "../Img/Lamp.jpeg", "../Img/SpiderMan.jpeg"]
 
 /* Declaring the alternative text for each image file */
 
@@ -18,6 +18,7 @@ const alts = {
     'Lamp.jpeg' : 'Lamp',
     'MoonLander.PNG' : 'MoonLander',
     'Telescope.PNG' : 'Telescope',
+    'Earth.jpg' : 'Earth',
     }
 
 /* Looping through images */
